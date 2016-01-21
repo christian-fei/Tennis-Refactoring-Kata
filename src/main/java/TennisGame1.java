1f8721035b95e5df0979739e6f415e30521b6eb3
Christian Fei <crifei93@gmail.com>
2016-01-21 21:22:06 +0100

    extracted formatAdvantageOrWinScore

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -24,15 +24,7 @@ public class TennisGame1 implements TennisGame {
 		if (isEven(firstPlayerScore, secondPlayerScore)) {
 			score = formatEvenScore();
 		} else if (isAdvantageOrWin(firstPlayerScore, secondPlayerScore)) {
-			int minusResult = firstPlayerScore - secondPlayerScore;
-			if (minusResult == 1)
-				score = "Advantage player1";
-			else if (minusResult == -1)
-				score = "Advantage player2";
-			else if (minusResult >= 2)
-				score = "Win for player1";
-			else
-				score = "Win for player2";
+			score = formatAdvantageOrWinScore();
 		} else {
 			for (int i = 1; i < 3; i++) {
 				if (i == 1)
@@ -57,6 +49,20 @@ public class TennisGame1 implements TennisGame {
 				}
 			}
 		}
+		return score;
+	}
+
+	private String formatAdvantageOrWinScore() {
+		String score;
+		int minusResult = firstPlayerScore - secondPlayerScore;
+		if (minusResult == 1)
+			score = "Advantage player1";
+		else if (minusResult == -1)
+			score = "Advantage player2";
+		else if (minusResult >= 2)
+			score = "Win for player1";
+		else
+			score = "Win for player2";
 		return score;
 	}
 
